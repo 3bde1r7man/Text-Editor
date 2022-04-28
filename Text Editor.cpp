ed388f305cd1ccfa4347f86ce4eff6bdb70558b0
Abdelrhman Mostafa <abdelrhman.m.hessain@gmail.com>
2022-04-28 10:34:47 +0200

    Merge pull request #1 from 3bde1r7man/ABDOfirst 5 funcs
    added the menuDiplay & Merge function & file1.txt & file2.txt

--- Text Editor.cpp
+++ Text Editor.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 int userInput;
 
+<<<<<<< Updated upstream
 // define each function are used
 void menuDisplay();
 void Merge();
@@ -11,6 +12,10 @@ void countWords();
 void countChars();
 void countLines();
 void searchWord();
+=======
+void Merge();
+void menuDisplay();
+>>>>>>> Stashed changes
 
 int main()
 {
@@ -21,6 +26,7 @@ int main()
     case 6:
         Merge();
         break;
+<<<<<<< Updated upstream
     case 7:
         countWords();
         break;
@@ -33,6 +39,8 @@ int main()
     case 10:
         searchWord();
         break;
+=======
+>>>>>>> Stashed changes
     default:
         break;
     }
@@ -40,7 +48,11 @@ int main()
 }
 //_________________________________________
 void menuDisplay() {
+<<<<<<< Updated upstream
     while (true) { // Dispaly menu and take user input
+=======
+    while (true) {
+>>>>>>> Stashed changes
         cout << "1- Add new text to the end of the file\n";
         cout << "2- Display the content of the file\n";
         cout << "3- Empty the file\n";
@@ -58,9 +70,14 @@ void menuDisplay() {
         cout << "15- Save\n";
         cout << "16- Exit\n--> ";
         cin >> userInput;
+<<<<<<< Updated upstream
         if (userInput >= 1 && userInput <= 16) {
             break;
         }
+=======
+        if (userInput >= 1 && userInput <= 16)
+            break;
+>>>>>>> Stashed changes
         else {
             cout << "Please enter valid input from the menu!!\n\n";
             cin.ignore();
@@ -71,6 +88,7 @@ void menuDisplay() {
 void Merge() {
     fstream firstFile, secFile;
     char name1[100], name2[100];
+<<<<<<< Updated upstream
     while (true)
     {
         cout << "Please enter the first file name to append to(e.g. file1.txt): ";
@@ -88,10 +106,25 @@ void Merge() {
             cout << "Please go check " << name1 << "\n";
             break;
         }
+=======
+    cout << "Please enter the first file name to append to(e.g. file1.txt): ";
+    cin >> name1;
+    cout << "Please enter the second file name(e.g. file2.txt): ";
+    cin >> name2;
+    firstFile.open(name1, ios::app);
+    secFile.open(name2, ios::in);
+    if (firstFile.fail() || secFile.fail()) {
+        cout << "please enter valid file name";
+    }
+    else {
+        firstFile << "\n" << secFile.rdbuf();
+        cout << "Please go check " << name1;
+>>>>>>> Stashed changes
     }
     firstFile.close();
     secFile.close();
 
+<<<<<<< Updated upstream
 }
 //_________________________________________
 void countWords() {
@@ -221,3 +254,6 @@ void searchWord() {
     file.close();
 }
 //_________________________________________
+=======
+}
+>>>>>>> Stashed changes
