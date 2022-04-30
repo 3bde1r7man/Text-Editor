d14847c515635f75cb8cd23959805e3153c2b618
youssef mohmed <youssefmohmedyahoo.com9@gmail.com>
2022-04-30 20:53:54 +0200

    Turn the file content to lower case

---
+++ lower_case.cpp
@@ -0,0 +1,18 @@
+void lower_case() {
+    char text_file;
+    string text_file2;
+    text_file2 = "";
+    ifstream myfile;
+    ofstream myfile2;
+    myfile.open(file_name + ".txt", ios::out | ios::app);
+    system("cls");
+    while (!myfile.eof()) {
+        myfile.get(text_file);
+        text_file2 = text_file2 + char(tolower(text_file));
+    }
+    myfile2.open(file_name + ".txt", ios::trunc);
+    myfile2 << text_file2;
+    cout << "Done!!" << endl;
+    cout << endl << endl;
+
+}
